725c54d8f99bddb8e99fe2194af0373926716e8d
Towfiqul Alam <piash5112@gmail.com>
2025-05-12 08:44:19 +0600

    Change 12

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     //show form for reg
     public function showRegister()
     {
-        return view('auth.register');
+        return view('Auth.register');
     }
 
     // Reg
@@ -48,7 +48,7 @@ class AuthController extends Controller
     //show login form
     public function showLogin()
     {
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     // login
